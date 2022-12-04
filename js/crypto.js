9c3c0bd86a4017af5d416b9c0ed11745024deb4f
itayyaakov1515 <itayy1515@gmail.com>
2022-12-04 16:46:34 +0200

    update API

--- js/crypto.js
+++ js/crypto.js
@@ -1,6 +1,29 @@
 window.onload = function(){
-     var new_coin = new Crypto(id_parent,"Bitcoin","bitcoin",3000)
-     new_coin.addToHTML()
-     var new_coin = new Crypto(id_parent,"Ethereum","ethereum",1800)
-     new_coin.addToHTML()   
+    //  var new_coin = new Crypto(id_parent,"Bitcoin","bitcoin",3000)
+    //  new_coin.addToHTML()
+    //  var new_coin = new Crypto(id_parent,"Ethereum","ethereum",1800)
+    //  new_coin.addToHTML()  
+    do_ajax() 
+}
+function do_ajax(){
+    var xmlHttp = new XMLHttpRequest()
+    xmlHttp.onreadystatechange = function(){
+        if(this.status == 200 && this.readyState == 4)
+        {
+            console.log(JSON.parse(this.response))
+            var coins_arr = JSON.parse(this.response);
+            creatAllCoins(coins_arr)
+        }
+    }
+    const url = "http://fs1.co.il/bus/bitcoin.php"
+    xmlHttp.open("GET",url,true)
+    xmlHttp.send()
+
+}
+function creatAllCoins(coins_arr){
+    for(var i = 0;i<coins_arr.length;i++){
+        var item = coins_arr[i]
+        var new_coin = new Crypto(id_parent,item.name,item.id,item.price_usd)
+        new_coin.addToHTML()
+    }
 }
